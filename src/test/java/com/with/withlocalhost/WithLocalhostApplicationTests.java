efa927c981276809a3596d405cd1c5434f247a55
ckdgns6078 <xpfjsdmlghkd@gmail.com>
2024-11-19 16:29:15 +0900

    chore :초기 프로젝트 세팅 및 properties 설정

---
+++ src/test/java/com/with/withlocalhost/WithLocalhostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.with.withlocalhost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithLocalhostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
