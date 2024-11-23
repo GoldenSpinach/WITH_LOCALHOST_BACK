47ef091f9855993a818f9ad8a59e4d306c456291
ckdgn <ckdgn@192.168.219.101>
2024-11-24 02:07:47 +0900

    hotfix: tour/create 어노테이션 수정

--- src/main/java/com/with/withlocalhost/tour/controller/RestTourController.java
+++ src/main/java/com/with/withlocalhost/tour/controller/RestTourController.java
@@ -85,7 +85,7 @@ public class RestTourController {
 
 	@Operation(summary = "tour 생성 ( insert )", description = "")
 	@PostMapping("/create")
-	public ResponseEntity<?> createTour(@ModelAttribute CreateTourDto tourdto,
+	public ResponseEntity<?> createTour(@RequestBody CreateTourDto tourdto,
 			@RequestPart(value = "mainImg" ,required = false) MultipartFile mainImg,
 			@RequestPart(value = "activityImg" , required = false) List<MultipartFile> activityImg) throws Exception {
 			tourService.createTour(tourdto, mainImg, activityImg);
